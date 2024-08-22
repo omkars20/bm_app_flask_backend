a0364f5e7080bc02222bf2a60ea6a95bf7a1972b
Omkar Singh <86602101+omkars20@users.noreply.github.com>
2024-08-22 17:55:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        user='wiomsudo',
-        password='wiomsudo',
-        host='34.131.53.25',  # Ensure this IP address is correct
-        database='wiomlabs',   # Ensure this database exists
+        user='your user name',
+        password='your password',
+        host='host name',  
+        database='database name',   
     )
     return connection
